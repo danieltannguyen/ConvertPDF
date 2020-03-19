163d05af3d6b16aa3ba3d6c7221cffa2fe1c1c35
codeskiller <danieltn91@gmail.com>
2020-03-18 21:25:28 -0700

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ConvertPDF
+{
+    partial class ConvertPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPdfFile = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtBoxOutputPath = new System.Windows.Forms.TextBox();
+            this.txtBoxOutputFile = new System.Windows.Forms.ComboBox();
+            this.labelOuput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPdfFile
+            // 
+            this.btnPdfFile.Location = new System.Drawing.Point(54, 25);
+            this.btnPdfFile.Name = "btnPdfFile";
+            this.btnPdfFile.Size = new System.Drawing.Size(106, 26);
+            this.btnPdfFile.TabIndex = 0;
+            this.btnPdfFile.Text = "Choose PDF file";
+            this.btnPdfFile.UseVisualStyleBackColor = true;
+            this.btnPdfFile.Click += new System.EventHandler(this.btnPdfFile_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(571, 28);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(56, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(43, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(718, 327);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // txtBoxOutputPath
+            // 
+            this.txtBoxOutputPath.Location = new System.Drawing.Point(465, 30);
+            this.txtBoxOutputPath.Name = "txtBoxOutputPath";
+            this.txtBoxOutputPath.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxOutputPath.TabIndex = 4;
+            this.txtBoxOutputPath.Text = "C:\\Output";
+            // 
+            // txtBoxOutputFile
+            // 
+            this.txtBoxOutputFile.FormattingEnabled = true;
+            this.txtBoxOutputFile.Items.AddRange(new object[] {
+            ".docx",
+            ".txt",
+            ".html"});
+            this.txtBoxOutputFile.Location = new System.Drawing.Point(266, 27);
+            this.txtBoxOutputFile.Name = "txtBoxOutputFile";
+            this.txtBoxOutputFile.Size = new System.Drawing.Size(121, 21);
+            this.txtBoxOutputFile.TabIndex = 5;
+            this.txtBoxOutputFile.Text = ".docx";
+            // 
+            // labelOuput
+            // 
+            this.labelOuput.AutoSize = true;
+            this.labelOuput.Location = new System.Drawing.Point(185, 30);
+            this.labelOuput.Name = "labelOuput";
+            this.labelOuput.Size = new System.Drawing.Size(75, 13);
+            this.labelOuput.TabIndex = 6;
+            this.labelOuput.Text = "Ouput file type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(409, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Save to: ";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(652, 16);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(109, 41);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // ConvertPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelOuput);
+            this.Controls.Add(this.txtBoxOutputFile);
+            this.Controls.Add(this.txtBoxOutputPath);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnPdfFile);
+            this.Name = "ConvertPDF";
+            this.Text = "Convert PDF Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPdfFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtBoxOutputPath;
+        private System.Windows.Forms.ComboBox txtBoxOutputFile;
+        private System.Windows.Forms.Label labelOuput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,75 @@
+﻿using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using System.IO;
+
+namespace ConvertPDF
+{
+    public partial class ConvertPDF : Form
+    {
+        private string path = "";
+        public ConvertPDF()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPdfFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = @"C:\";
+            file.Filter = "PDF file only (*.pdf)|*.pdf";
+            file.FilterIndex = 1;
+            file.Multiselect = false;
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = file.FileName;
+                FileInfo f = new FileInfo(fileName);
+                path = f.DirectoryName;
+                //set output path
+                txtBoxOutputPath.Text = path;
+            }
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxOutputPath.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+        private void Convert()
+        {
+            PDDocument doc = null;
+            doc = PDDocument.load(@"C:\test\test.pdf");
+            PDFTextStripper textStrip = new PDFTextStripper();
+            string strPDFText = textStrip.getText(doc);
+            doc.close();
+
+            //define the path where you want to save the doc file
+            string fn = @"C:\test\test.docx";
+            var wordDoc = DocX.Create(fn);
+
+            //insert data in it
+            wordDoc.InsertParagraph(strPDFText);
+
+            //save the word file
+            wordDoc.Save();
+
+            //open the word file
+            Process.Start("WINWORD.EXE", fn);
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        
+    }
+}
